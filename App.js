54e95cbfc3bd0218ca9cfd8d2ad4366def7af7c3
Fayaz <apple@Fayazs-MacBook-Air.local>
2023-09-06 14:38:01 -0400

    episode1

---
+++ App.js
@@ -0,0 +1,10 @@
+const parent = React.createElement("div", { id: "div" }, [
+  React.createElement(
+    "h1",
+    {},
+    "Welcome to JavaScript"),
+    React.createElement("h2", {}, "Welcome to JavaScript2"),]),
+    ;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
